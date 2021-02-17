4a33992d9a15abd70a5b29b1387125e0aa6eff49
hville <hugov@runbox.com>
2021-02-16 21:32:44 -0500

    var => let const

--- index.js
+++ index.js
@@ -13,15 +13,7 @@ export default class D {
 	get N() { return this.rs[this.rs.length-1] }
 
 	// Expected Value
-	get E() {
-		const vs = this.vs,
-					rs = this.rs,
-					N = Math.min(rs.length, rs[rs.length-1]), // covers the case when the buffer is not yet full
-					M = N-1
-		let sum = vs[0] + vs[M] // correction at edge to match actual discrete result
-		for (let i=0; i<M; ++i) sum += (vs[i+1] + vs[i]) * (rs[i+1] - rs[i])
-		return sum / 2 / rs[M]
-	}
+	get E() { return this.Σ(1) / this.N }
 
 	// Variance
 	get V() {
@@ -39,22 +31,22 @@ export default class D {
 	 * https://en.wikipedia.org/wiki/Continuous_uniform_distribution#Moments
 	 *
 	 * @param {number} order
-	 * @return {number} E( X^order )
+	 * @return {number} Σ( X^pow )
 	 */
-	 Σ(exp) {
+	 Σ(pow) {
 		const vs = this.vs,
 					rs = this.rs,
-					M = Math.min(rs.length, rs[rs.length-1]),
+					M = Math.min(rs.length, rs[rs.length-1]), // in case the buffer is not full
 					Mm = M-1,
-					Op = order + 1
-		if (exp === 0) return rs[Mm]
-		if (exp === 1) {
-			let sum = vs[0] + vs[M] // correction at edge to match actual discrete result
-			for (let i=0; i<M; ++i) sum += (vs[i+1] + vs[i]) * (rs[i+1] - rs[i])
-			return sum / 2
+					Op = pow + 1
+		if (pow === 0) return rs[Mm]
+		if (pow === 1) { //same as below but simplified
+			let sum = vs[0] + vs[Mm] // correction at edge to match actual discrete result
+			for (let i=0; i<Mm; ++i) sum += (vs[i+1] + vs[i]) * (rs[i+1] - rs[i])
+			return sum / Op
 		}
-		let sum = ( Math.pow(vs[0], order) + Math.pow(vs[Mm], order) ) * Op / 2
-		for (var i=0; i<Mm; ++i) sum += (rs[i+1] - rs[i]) * ( Math.pow(vs[i+1], Op) - Math.pow(vs[i], Op) ) / (vs[i+1] - vs[i])
+		let sum = ( Math.pow(vs[0], pow) + Math.pow(vs[Mm], pow) ) * Op / 2 //TODO can taper edges instead of square
+		for (let i=0; i<Mm; ++i) sum += (rs[i+1] - rs[i]) * ( Math.pow(vs[i+1], Op) - Math.pow(vs[i], Op) ) / (vs[i+1] - vs[i])
 		return sum / Op
 	}
 
@@ -65,16 +57,7 @@ export default class D {
 	 * @param {number} order
 	 * @return {number} E( X^order )
 	 */
-	M(order) {
-		const vs = this.vs,
-					rs = this.rs,
-					M = Math.min(rs.length, rs[rs.length-1]),
-					Mm = M-1,
-					Op = order + 1
-		let sum = ( Math.pow(vs[0], order) + Math.pow(vs[Mm], order) ) * Op / 2
-		for (var i=0; i<Mm; ++i) sum += (rs[i+1] - rs[i]) * ( Math.pow(vs[i+1], Op) - Math.pow(vs[i], Op) ) / (vs[i+1] - vs[i])
-		return sum / Op / rs[Mm]
-	}
+	M(order) { return this.Σ(order) / this.N }
 
 	/**
 	 * Quantile function, provide the value for a given probability
@@ -82,12 +65,12 @@ export default class D {
 	 * @return {number} value or array of values
 	 */
 	Q(prob) {
-		var vs = this.vs,
-				rs = this.rs,
-				M = Math.min(rs.length, rs[rs.length-1]),
-				h = rs[M-1] * prob + 0.5, // 0.5 <= h <= N+0.5
-				j = topIndex(rs, h, M), //      0 <= j <= M
-				i = j-1
+		const vs = this.vs,
+					rs = this.rs,
+					M = Math.min(rs.length, rs[rs.length-1]), // in case the buffer is not full
+					h = rs[M-1] * prob + 0.5, // 0.5 <= h <= N+0.5
+					j = topIndex(rs, h, M), //      0 <= j <= M
+					i = j-1
 		return j === 0 ? vs[0]
 			: j === M ? vs[M-1]
 			: vs[i] + (vs[j] - vs[i]) * (h-rs[i]) / (rs[j]-rs[i])
@@ -98,12 +81,12 @@ export default class D {
 	 * @return {number} value or array of values
 	 */
 	F(x) {
-		var vs = this.vs,
-				rs = this.rs,
-				M = Math.min(rs.length, rs[rs.length-1]),
-				N = rs[M-1],
-				j = topIndex(vs, x, M),
-				i = j-1
+		const vs = this.vs,
+					rs = this.rs,
+					M = Math.min(rs.length, rs[rs.length-1]), // in case the buffer is not full
+					N = rs[M-1],
+					j = topIndex(vs, x, M),
+					i = j-1
 		return (j === 0 ? 0.5
 			: j === M ? (N - 0.5)
 			: rs[i] - 0.5 + (rs[j] - rs[i]) * (x - vs[i]) / (vs[j] - vs[i])
@@ -115,43 +98,41 @@ export default class D {
 	 * @return {number} value or array of values
 	 */
 	f(x) {
-		var vs = this.vs,
-				rs = this.rs,
-				M = Math.min(rs.length, rs[rs.length-1]),
-				N = rs[M-1]
+		const vs = this.vs,
+					rs = this.rs,
+					M = Math.min(rs.length, rs[rs.length-1]),
+					N = rs[M-1]
 		if (x === vs[0] || x === vs[M-1]) return 0.5/N
-		var j = topIndex(vs, x, M),
-				i = j-1
+		const j = topIndex(vs, x, M),
+					i = j-1
 		return j === 0 || j === M ? 0 : (rs[j] - rs[i]) / (vs[j] - vs[i]) / N
 	}
 
 	push(x) {
-		var vs = this.vs,
-				rs = this.rs,
-				M = Math.min(rs.length, rs[rs.length-1]),
-				j = topIndex(this.vs, x, M),
-				ir = 0
+		const vs = this.vs,
+					rs = this.rs,
+					M = Math.min(rs.length, rs[rs.length-1]),
+					j = topIndex(this.vs, x, M)
 		// lossless insert
 		if (M < rs.length) {
-			for (ir=M; ir>j; --ir) {
+			for (let ir=M; ir>j; --ir) {
 				rs[ir] = ir+1
 				vs[ir] = vs[ir-1]
 			}
 			rs[j] = j ? rs[j-1] + 1 : 1
 			vs[j] = x
-			//console.log(j, rs)
 			if (M!==rs.length-1) ++rs[rs.length-1]
 		}
 		// compression, droping a value while maintaining the interval average
 		else if (j === M) newmax(vs, rs, x)
 		else if (j === 0) {
-			var u = vs[0],
-					Δwx = vs[2] - x
+			const u = vs[0],
+						Δwx = vs[2] - x
 			if (Δwx !== 0) {
 				//KEEP 0 : r'(w-x) = (w+v-2x) + r(w-u) - s(v-u)
 				//DROP 0 : r'(w-x) = (w+v-2x) + r(w-u)
-				var r_v = (vs[2] + vs[1] - 2*x + rs[1]*(vs[2] - u)) / Δwx,
-						r_u = r_v - rs[2] * (vs[1]-u) / Δwx
+				const r_v = (vs[2] + vs[1] - 2*x + rs[1]*(vs[2] - u)) / Δwx,
+							r_u = r_v - rs[2] * (vs[1]-u) / Δwx
 				if ( (u + vs[1] > x + vs[2] && r_u > rs[0]) || r_v > rs[2]+1) {
 					vs[1] = u
 					rs[1] = r_u
@@ -159,15 +140,15 @@ export default class D {
 				else rs[1] = r_v
 				vs[0] = x
 			}
-			for (ir=2; ir<rs.length; ++ir) ++rs[ir]
+			for (let ir=2; ir<rs.length; ++ir) ++rs[ir]
 		}
 		else {
-			var i = j === 1 || (j !== M-1 && 2*x > vs[j]+vs[j-1] ) ? j : j-1
-			var w = vs[i+1],
-					v = vs[i],
-					Δwu = w-vs[i-1]
+			let i = j === 1 || (j !== M-1 && 2*x > vs[j]+vs[j-1] ) ? j : j-1
+			const w = vs[i+1],
+						v = vs[i],
+						Δwu = w-vs[i-1]
 			if (Δwu !== 0) {
-				var r_x = rs[i] + ( w-x + (x-v)*(rs[i+1] - rs[i-1]) ) / Δwu
+				const r_x = rs[i] + ( w-x + (x-v)*(rs[i+1] - rs[i-1]) ) / Δwu
 				if ( v < x
 					? vs[i-1]+w < v+x || r_x > rs[i+1]+1
 					: v+x < vs[i-1] + w || r_x < rs[i-1]
@@ -183,15 +164,15 @@ export default class D {
 }
 
 function newmax(vs, rs, x) {
-	var j = rs.length-1,
-			i = j-1,
-			h = i-1,
-			Δwv = vs[j]-vs[i],
-			Δxu = x - vs[h],
-			rjh = rs[i]*(vs[j]-vs[h])
+	const j = rs.length-1,
+				i = j-1,
+				h = i-1,
+				Δwv = vs[j]-vs[i],
+				Δxu = x - vs[h],
+				rjh = rs[i]*(vs[j]-vs[h])
 	if (Δxu !== 0) {
-		var r_w = ( rs[j]*(x-vs[i]) + rjh - rs[h]*Δwv ) / Δxu,
-				r_v = ( rs[j]*(x-vs[j]) + rjh - Δwv ) / Δxu
+		const r_w = ( rs[j]*(x-vs[i]) + rjh - rs[h]*Δwv ) / Δxu,
+					r_v = ( rs[j]*(x-vs[j]) + rjh - Δwv ) / Δxu
 		if ( r_v < rs[h] || (vs[i] + vs[j] < vs[h] + x && r_w < rs[j]+1)) {
 			vs[i] = vs[j]
 			rs[i] = r_w
@@ -203,9 +184,9 @@ function newmax(vs, rs, x) {
 }
 
 function topIndex(arr, v, max) {
-	var low = 0
+	let low = 0
 	while (low < max) {
-		var mid = (low + max) >>> 1
+		const mid = (low + max) >>> 1
 		if (arr[mid] < v) low = mid + 1
 		else max = mid
 	}
